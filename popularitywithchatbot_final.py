422e9f5ec339866ae9afc77d62b25a3bf3d9d94e
Jes <143019142+JesHP73@users.noreply.github.com>
2023-11-09 18:56:38 +0100

    Add files via upload

--- popularitywithchatbot_final.py
+++ popularitywithchatbot_final.py
@@ -24,38 +24,48 @@ def popularity_recommender(df, num_recommendations):
     sorted_movies = df.sort_values(by='popularity_score', ascending=False)
     # Get the top 'num_recommendations' movies
     recommended_movies = sorted_movies.head(num_recommendations)
-    return recommended_movies
-
-# Define the genres
-genres = ['Comedy', 'Drama', 'Thriller']
+    return recommended_movies[['movieId', 'title', 'genres']]  # Removed the popularity score from the output
 
 def run_streamlit_app():
     # Load the movie data from the GitHub URL
     df = pd.read_csv(csv_url)
 
     # Streamlit app
-    st.title("Personal movie Recommender")
-    st.write("Hi! I'm your personal recommender.")
+    st.title("Personal Movie Recommender")
+    st.write("Hi! I'm your personal recommender. 😊🎬")
 
-    genre = st.radio("Choose your genre:", genres)
+    # Use a chat message container
+    chat = st.container()
 
-    if genre:
-        st.write(f"You have chosen {genre}")
-        recommended_movies = popularity_recommender(df, 15)
-        if genre == 'Comedy':
-            a = recommended_movies[recommended_movies["genres"].str.contains("Comedy")]
-        elif genre == 'Drama':
-            a = recommended_movies[recommended_movies["genres"].str.contains("Drama")]
-        elif genre == 'Thriller':
-            a = recommended_movies[recommended_movies["genres"].str.contains("Thriller")]
-        else:
-            st.write("Invalid genre selection")
+    genres = ['Comedy', 'Drama', 'Thriller']
+    genre_choice = st.text_input("Choose your genre (type 1 for Comedy, 2 for Drama, 3 for Thriller):")
 
-        if not a.empty:
-            st.write("Recommended movies:")
-            st.table(a[['title', 'popularity_score']].reset_index(drop=True))
-        else:
-            st.write("No recommendations found for this genre.")
+    # Mapping user input to genre names
+    genre_map = {'1': 'Comedy', '2': 'Drama', '3': 'Thriller'}
+
+    if genre_choice in genre_map:
+        selected_genre = genre_map[genre_choice]
+        with chat:
+            st.chat_message("user", f"You have chosen {selected_genre}")
+            top_movies = popularity_recommender(df, 10)
+
+            a = top_movies[top_movies["genres"].str.contains(selected_genre)] 
+
+            if not a.empty:
+                st.chat_message("assistant", "Here are your personal movie recommendations, Enjoy! 🍿")
+                for movie_title in a['title']:
+                    st.chat_message("assistant", movie_title)
+            else:
+                st.chat_message("assistant", "No recommendations found now, sorry. 😕")
+
+        continue_chat = st.radio("Do you want another recommendation?", ["Yes", "No"])
+        if continue_chat == "No":
+            with chat:
+                st.chat_message("assistant", "Thanks for using the recommender! Have a great day! 👋")
+            st.stop()
+    else:
+        with chat:
+            st.chat_message("assistant", "Invalid genre selection. Please type 1, 2, or 3. 🔄")
 
 if __name__ == "__main__":
     run_streamlit_app()

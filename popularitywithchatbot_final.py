323a737b85f5e947832df808fad0aa3b6f4d7671
Jes <143019142+JesHP73@users.noreply.github.com>
2023-11-09 19:00:01 +0100

    Update popularitywithchatbot_final.py

--- popularitywithchatbot_final.py
+++ popularitywithchatbot_final.py
@@ -3,44 +3,36 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# Define the GitHub URL for the raw CSV data
+
 csv_url = "https://raw.githubusercontent.com/JesHP73/moviereco/85845a1166f211909985e551c8ab860ce58727b5/sample_movies.csv"
 
-# Redefine the functions with the modifications
+
 def calculate_popularity(df):
-    # Calculate the count of ratings for each movie
     ratings_count = df.groupby('movieId')['rating'].count()
-    # Calculate the average rating for each movie
     average_ratings = df.groupby('movieId')['rating'].mean()
-    # Calculate the popularity score as the product of average rating and the logarithm of the count of ratings
     popularity_score = average_ratings * np.log1p(ratings_count)
-    # Return the popularity score mapped to the movieId
     return popularity_score.to_dict()
 
 def popularity_recommender(df, num_recommendations):
-    # Calculate the popularity score for each movie
     df['popularity_score'] = df['movieId'].map(calculate_popularity(df))
-    # Sort the movies by the calculated popularity score in descending order
     sorted_movies = df.sort_values(by='popularity_score', ascending=False)
-    # Get the top 'num_recommendations' movies
     recommended_movies = sorted_movies.head(num_recommendations)
-    return recommended_movies[['movieId', 'title', 'genres']]  # Removed the popularity score from the output
+    return recommended_movies[['movieId', 'title', 'genres']]  # took out the 'rating' for the user
 
 def run_streamlit_app():
-    # Load the movie data from the GitHub URL
+    # Load the movie data from the GitHub URL and lest call it df
     df = pd.read_csv(csv_url)
 
-    # Streamlit app
+    # Streamlit app starts from here  for the user 
     st.title("Personal Movie Recommender")
     st.write("Hi! I'm your personal recommender. 😊🎬")
 
-    # Use a chat message container
+    
     chat = st.container()
 
     genres = ['Comedy', 'Drama', 'Thriller']
     genre_choice = st.text_input("Choose your genre (type 1 for Comedy, 2 for Drama, 3 for Thriller):")
-
-    # Mapping user input to genre names
+    
     genre_map = {'1': 'Comedy', '2': 'Drama', '3': 'Thriller'}
 
     if genre_choice in genre_map:
